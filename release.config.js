e4f3c57ca5baa27416879fdd87a695bcec7c5cc3
luigi-borriello00 <luigi.borriello2@studio.unibo.it>
2024-02-08 17:37:31 +0100

    feat: add semantic release plugin

--- release.config.js
+++ release.config.js
@@ -1,17 +1,7 @@
-var publishCmd = `
-IMAGE_NAME="danysk/docker-manjaro-texlive-ruby"
-docker build -t "$IMAGE_NAME:\${nextRelease.version}"
-docker push --all-tags "$IMAGE_NAME"
-`
 var config = require('semantic-release-preconfigured-conventional-commits');
 config.plugins.push(
-    [
-        "@semantic-release/exec",
-        {
-            "publishCmd": publishCmd,
-        }
-    ],
     "@semantic-release/github",
     "@semantic-release/git",
 )
 module.exports = config
+
