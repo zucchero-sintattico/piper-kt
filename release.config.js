32b780f001b9b1ca1b7356d01e512645ddbac404
luigi-borriello00 <luigi.borriello2@studio.unibo.it>
2024-02-08 11:02:21 +0100

    feat(semantic-release): move to prof's plugin

--- release.config.js
+++ release.config.js
@@ -1,22 +1,17 @@
-const config = {
-    branches: ['main'],
-    plugins: [
-        // Looks for commits that are breaking changes, features, or fixes
-        '@semantic-release/commit-analyzer',
-        // Generates the release notes
-        '@semantic-release/release-notes-generator',
-        // Updates the version in package.json
-        [
-            '@semantic-release/git',
-            {
-                assets: ['CHANGELOG.md', 'package.json'],
-                // This commit doesn't trigger ci/cd
-                message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
-            }
-        ],
-        // Publishes the release to GitHub
-        '@semantic-release/github',
-    ]
-};
-
-module.exports = config;
+var publishCmd = `
+IMAGE_NAME="danysk/docker-manjaro-texlive-ruby"
+docker build -t "$IMAGE_NAME:\${nextRelease.version}"
+docker push --all-tags "$IMAGE_NAME"
+`
+var config = require('semantic-release-preconfigured-conventional-commits');
+config.plugins.push(
+    [
+        "@semantic-release/exec",
+        {
+            "publishCmd": publishCmd,
+        }
+    ],
+    "@semantic-release/github",
+    "@semantic-release/git",
+)
+module.exports = config
