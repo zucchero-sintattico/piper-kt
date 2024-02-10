699916603f0f65a97c5bf4dfbee490fc445c072c
luigi-borriello00 <luigi.borriello2@studio.unibo.it>
2024-02-10 16:44:43 +0100

    feat: add changelog plugin

--- release.config.js
+++
@@ -1,7 +0,0 @@
-var config = require('semantic-release-preconfigured-conventional-commits');
-config.plugins.push(
-    "@semantic-release/github",
-    "@semantic-release/git",
-)
-module.exports = config
-
