a5435c43dd011b922e9d32c2a8cae0d72bce61db
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2024-03-09 18:15:57 +0100

    fix(test): fixed architecture test

--- multimedia-service/src/test/kotlin/architecture/ArchitectureSpec.kt
+++ multimedia-service/src/test/kotlin/architecture/ArchitectureSpec.kt
@@ -5,9 +5,7 @@ import com.lemonappdev.konsist.api.architecture.DependencyRules
 import com.lemonappdev.konsist.api.architecture.KoArchitectureCreator.assertArchitecture
 import com.lemonappdev.konsist.api.architecture.Layer
 import io.kotest.core.spec.style.AnnotationSpec
-import io.micronaut.test.extensions.kotest5.annotation.MicronautTest
 
-@MicronautTest
 open class ArchitectureSpec(private val prefix: String) : AnnotationSpec() {
     fun asserLayer(name: String): Layer {
         return Layer(name, "$prefix.$name..")
