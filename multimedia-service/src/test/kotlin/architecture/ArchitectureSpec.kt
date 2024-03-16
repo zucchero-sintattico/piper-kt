e51c5dd597126a021db5d5897d0ccca8180b30c5
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2024-03-16 18:27:27 +0100

    test(architecture): add framework indipendece architecture tests

--- multimedia-service/src/test/kotlin/architecture/ArchitectureSpec.kt
+++ multimedia-service/src/test/kotlin/architecture/ArchitectureSpec.kt
@@ -4,6 +4,7 @@ import com.lemonappdev.konsist.api.Konsist
 import com.lemonappdev.konsist.api.architecture.DependencyRules
 import com.lemonappdev.konsist.api.architecture.KoArchitectureCreator.assertArchitecture
 import com.lemonappdev.konsist.api.architecture.Layer
+import io.kotest.matchers.shouldBe
 
 interface ArchitectureSpec {
 
@@ -15,5 +16,20 @@ interface ArchitectureSpec {
 
     fun assertArchitecture(block: DependencyRules.() -> Unit) {
         Konsist.scopeFromProject().assertArchitecture { block() }
+    }
+
+    fun assertPackageDoesNotDependOnFrameworks(
+        packageName: String,
+        frameworks: List<String> = emptyList()
+    ) {
+        Konsist.scopeFromPackage("$prefix.$packageName", sourceSetName = "main").files.forEach {
+            file ->
+            val dependencies = file.imports.map { it.name }
+            val forbidden =
+                dependencies.filter { dependency ->
+                    frameworks.any { framework -> dependency.startsWith(framework) }
+                }
+            forbidden shouldBe emptyList()
+        }
     }
 }
