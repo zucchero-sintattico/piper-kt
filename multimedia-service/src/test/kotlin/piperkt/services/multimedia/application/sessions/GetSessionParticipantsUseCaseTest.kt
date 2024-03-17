6dcab23173b16cdf5379c91da9318f4af0463dba
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2024-03-17 13:34:47 +0100

    refactor(api): Renamed GetUsersInSession to GetSessionParticipants for clarity. Updates use cases and controller to reflect the change.

---
+++ multimedia-service/src/test/kotlin/piperkt/services/multimedia/application/sessions/GetSessionParticipantsUseCaseTest.kt
@@ -0,0 +1,37 @@
+package piperkt.services.multimedia.application.sessions
+
+import base.MicronautTest
+import data.SessionsData
+import io.kotest.matchers.shouldBe
+import mocks.repositories.SessionRepositoryMocks
+import piperkt.services.multimedia.domain.sessions.SessionId
+
+class GetSessionParticipantsUseCaseTest :
+    MicronautTest({
+        val session = SessionsData.simpleSession()
+        val sessionRepository = SessionRepositoryMocks.fromSessions(session)
+        val getSessionParticipantsUseCase = GetSessionParticipantsUseCase(sessionRepository)
+
+        test("should return users when session exists") {
+            val result =
+                getSessionParticipantsUseCase.handle(
+                    GetSessionParticipantsUseCase.Query(session.id.value)
+                )
+
+            result.isSuccess shouldBe true
+            result.getOrNull()?.users shouldBe
+                session.participants.map { it.username.value }.toSet()
+        }
+
+        test("should return SessionNotFound error when session does not exist") {
+            val fakeSessionId = SessionId("fakeSessionId")
+            val result =
+                getSessionParticipantsUseCase.handle(
+                    GetSessionParticipantsUseCase.Query(fakeSessionId.value)
+                )
+
+            result.isFailure shouldBe true
+            result.exceptionOrNull() shouldBe
+                GetSessionParticipantsUseCase.Errors.SessionNotFound(fakeSessionId.value)
+        }
+    })
