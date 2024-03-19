f26c586e829735e198c48ee14a11cede6d42671d
Tommaso Patriti <tommaso.patriti@studio.unibo.it>
2024-03-19 17:08:29 +0100

    feat(FriendsControllerTest): add test case for getFriendRequests endpoint to return friend requests of authenticated user

--- friendship-service/src/main/kotlin/piperkt/services/friendship/infrastructure/web/FriendsHTTPController.kt
+++ friendship-service/src/main/kotlin/piperkt/services/friendship/infrastructure/web/FriendsHTTPController.kt
@@ -1,7 +1,11 @@
 package piperkt.services.friendship.infrastructure.web
 
 import io.micronaut.http.MediaType.TEXT_PLAIN
-import io.micronaut.http.annotation.*
+import io.micronaut.http.annotation.Body
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Post
+import io.micronaut.http.annotation.Produces
 import io.micronaut.security.annotation.Secured
 import io.micronaut.security.authentication.Authentication
 import io.micronaut.security.rules.SecurityRule
