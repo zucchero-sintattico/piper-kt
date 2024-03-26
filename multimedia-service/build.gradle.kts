5ff562cf55b86eb0a9d78e45e9036e6157adf397
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2024-03-26 18:24:18 +0100

    feat(deps): added socketio-client in order to test the socket.io server

--- multimedia-service/build.gradle.kts
+++ multimedia-service/build.gradle.kts
@@ -10,6 +10,7 @@ dependencies {
     implementation("io.micronaut.kafka:micronaut-kafka")
     testImplementation("org.mockito.kotlin:mockito-kotlin:5.2.1")
     implementation("com.corundumstudio.socketio:netty-socketio:2.0.9")
+    implementation("io.socket:socket.io-client:2.1.0")
 }
 
 detekt { config.from(files("detekt-config.yml")) }
--- multimedia-service/src/test/kotlin/piperkt/services/multimedia/interfaces/websockets/server/MultimediaSocketIOServerTest.kt
+++ multimedia-service/src/test/kotlin/piperkt/services/multimedia/interfaces/websockets/server/MultimediaSocketIOServerTest.kt
@@ -2,10 +2,15 @@ package piperkt.services.multimedia.interfaces.websockets.server
 
 import base.Test
 import io.kotest.core.spec.style.FunSpec
+import io.socket.client.IO
 
 class MultimediaSocketIOServerTest :
     Test.Unit,
     FunSpec({
         val server = MultimediaSocketIOServer(SocketIOConfiguration())
-        test("should start server") { server.start() }
+        server.start()
+        val client = IO.socket("http://localhost:8888")
+        client.connect()
+
+        test("should handle join message") { client.emit("join", "john") }
     })
