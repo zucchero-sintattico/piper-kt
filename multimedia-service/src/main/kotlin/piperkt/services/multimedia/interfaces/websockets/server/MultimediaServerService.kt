b40c6c734e1940c0b325e17761b40d5a59f72260
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2024-03-27 14:29:07 +0100

    feat(ws): added token extraction and rooms managment

--- multimedia-service/src/main/kotlin/piperkt/services/multimedia/interfaces/websockets/server/MultimediaServerService.kt
+++ multimedia-service/src/main/kotlin/piperkt/services/multimedia/interfaces/websockets/server/MultimediaServerService.kt
@@ -1,59 +1,103 @@
 package piperkt.services.multimedia.interfaces.websockets.server
 
 import com.corundumstudio.socketio.Configuration
+import com.corundumstudio.socketio.SocketIOClient
 import com.corundumstudio.socketio.SocketIOServer
 import io.micronaut.context.annotation.ConfigurationProperties
-import jakarta.inject.Singleton
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.Json
-import piperkt.services.multimedia.interfaces.websockets.api.MultimediaServerApi
-
-class MultimediaServerService : MultimediaServerApi {
-
-    override fun onUserJoin(username: String, sessionId: String) {
-        TODO("Not yet implemented")
-    }
-
-    override fun onUserLeave(username: String, sessionId: String) {
-        TODO("Not yet implemented")
-    }
-
-    override fun onOffer(username: String, sessionId: String, offer: String) {
-        TODO("Not yet implemented")
-    }
-
-    override fun onAnswer(username: String, sessionId: String, answer: String) {
-        TODO("Not yet implemented")
-    }
-
-    override fun onIceCandidate(username: String, sessionId: String, candidate: String) {
-        TODO("Not yet implemented")
-    }
-}
+import piperkt.services.multimedia.interfaces.on
+import piperkt.services.multimedia.interfaces.websockets.api.MultimediaProtocolMessage
 
 @ConfigurationProperties("socketio")
 class SocketIOConfiguration {
     var port: Int = 8888
 }
 
-@Serializable data class JoinMessage(val username: String, val content: String)
-
-@Singleton
-class MultimediaSocketIOServer(private val configuration: SocketIOConfiguration) {
-    // Setup the server to use JSON for messages
-    private val socketIoConfig = Configuration().apply { port = configuration.port }
+class MultimediaSocketIOServer(
+    private val configuration: SocketIOConfiguration = SocketIOConfiguration()
+) {
+    private val socketIoConfig =
+        Configuration().apply { port = configuration.port }.apply { isNeedClientAuth = false }
     private val server = SocketIOServer(socketIoConfig)
+    private val clients = mutableMapOf<String, SocketIOClient>()
+    private val authorizedUsers = mutableSetOf<String>()
 
     fun start() {
-        server.addConnectListener { client -> println("connected: $client") }
-        server.addDisconnectListener { println("disconnect") }
-        server.addEventListener("join", String::class.java) { client, data, ackRequest ->
-            val joinMessage = Json.decodeFromString(JoinMessage.serializer(), data)
-            println("join: $joinMessage")
+        server.addConnectListener(this::onConnect)
+        server.addDisconnectListener(this::onDisconnect)
+        server.on("join") { client, message: MultimediaProtocolMessage.JoinMessage, _ ->
+            onJoin(client, message)
         }
-        server.addEventListener("leave", String::class.java) { client, data, ackRequest ->
-            println("leave")
+        server.on("offer") { _, message: MultimediaProtocolMessage.OfferMessage, _ ->
+            onOffer(message)
+        }
+        server.on("answer") { _, message: MultimediaProtocolMessage.AnswerMessage, _ ->
+            onAnswer(message)
+        }
+        server.on("iceCandidate") { _, message: MultimediaProtocolMessage.IceCandidateMessage, _ ->
+            onIceCandidate(message)
         }
         server.start()
+    }
+
+    private fun SocketIOClient.getUsername(): String {
+        return this.handshakeData.httpHeaders.get("authToken")
+    }
+
+    private fun roomOf(sessionId: String) = server.getRoomOperations(sessionId)
+
+    fun onConnect(client: SocketIOClient) {
+        val username = client.getUsername()
+        clients[username] = client
+        authorizedUsers.add(username)
+        println("User $username connected")
+    }
+
+    fun onDisconnect(client: SocketIOClient) {
+        val username = client.getUsername()
+        clients.remove(username)
+        authorizedUsers.remove(username)
+        println("User $username disconnected")
+    }
+
+    fun onJoin(
+        client: SocketIOClient,
+        joinMessage: MultimediaProtocolMessage.JoinMessage,
+    ) {
+        val username = client.getUsername()
+        if (!authorizedUsers.contains(username)) {
+            client.disconnect()
+            return
+        }
+        val sessionId = joinMessage.sessionId
+        roomOf(sessionId).sendEvent("userJoin", username)
+        client.joinRoom(sessionId)
+        println("User $username joined session $sessionId")
+    }
+
+    fun onOffer(
+        offerMessage: MultimediaProtocolMessage.OfferMessage,
+    ) {
+        val to = offerMessage.to
+        val toClient = clients[to] ?: return
+        toClient.sendEvent("offer", offerMessage)
+        println("User ${offerMessage.from} sent offer to $to")
+    }
+
+    fun onAnswer(
+        answerMessage: MultimediaProtocolMessage.AnswerMessage,
+    ) {
+        val to = answerMessage.to
+        val toClient = clients[to] ?: return
+        toClient.sendEvent("answer", answerMessage)
+        println("User ${answerMessage.from} sent answer to $to")
+    }
+
+    fun onIceCandidate(
+        iceCandidateMessage: MultimediaProtocolMessage.IceCandidateMessage,
+    ) {
+        val to = iceCandidateMessage.to
+        val toClient = clients[to] ?: return
+        toClient.sendEvent("iceCandidate", iceCandidateMessage)
+        println("User ${iceCandidateMessage.from} sent ice candidate to $to")
     }
 }
