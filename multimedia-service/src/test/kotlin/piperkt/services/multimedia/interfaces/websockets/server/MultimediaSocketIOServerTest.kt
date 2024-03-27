9b6ea80c84e224c06f0450d3867c462db1de4fc8
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2024-03-27 15:12:27 +0100

    test: added events test from socketio server

--- multimedia-service/src/test/kotlin/piperkt/services/multimedia/interfaces/websockets/server/MultimediaSocketIOServerTest.kt
+++ multimedia-service/src/test/kotlin/piperkt/services/multimedia/interfaces/websockets/server/MultimediaSocketIOServerTest.kt
@@ -2,7 +2,11 @@ package piperkt.services.multimedia.interfaces.websockets.server
 
 import base.Test
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
 import io.socket.client.IO
+import piperkt.services.multimedia.interfaces.toJson
+import piperkt.services.multimedia.interfaces.websockets.api.MultimediaProtocolMessage
+import piperkt.services.multimedia.interfaces.websockets.api.MultimediaProtocolMessage.JoinMessage
 
 class MultimediaSocketIOServerTest :
     Test.Unit,
@@ -15,6 +19,33 @@ class MultimediaSocketIOServerTest :
                 IO.Options.builder().setExtraHeaders(mapOf("authToken" to listOf("test"))).build()
             )
         client.connect()
+        Thread.sleep(1000)
 
-        test("should start the server") {}
+        test("should allow client to join") {
+            val message = JoinMessage("sessionId")
+            client.emit("join", message.toJson())
+            Thread.sleep(1000)
+            server.events.last() shouldBe message
+        }
+
+        test("should allow client to offer") {
+            val message = MultimediaProtocolMessage.OfferMessage("from", "to", "offer")
+            client.emit("offer", message.toJson())
+            Thread.sleep(1000)
+            server.events.last() shouldBe message
+        }
+
+        test("should allow client to answer") {
+            val message = MultimediaProtocolMessage.AnswerMessage("from", "to", "answer")
+            client.emit("answer", message.toJson())
+            Thread.sleep(1000)
+            server.events.last() shouldBe message
+        }
+
+        test("should allow client to send ice candidate") {
+            val message = MultimediaProtocolMessage.IceCandidateMessage("from", "to", "candidate")
+            client.emit("candidate", message.toJson())
+            Thread.sleep(1000)
+            server.events.last() shouldBe message
+        }
     })
