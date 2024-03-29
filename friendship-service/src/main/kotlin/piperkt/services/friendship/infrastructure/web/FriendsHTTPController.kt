ee7566a37f21078aadb20004831af47a9ca20f9f
Tommaso Patriti <tommaso.patriti@studio.unibo.it>
2024-03-29 16:10:59 +0100

    chore(FriendsHTTPController): remove @Produces(TEXT_PLAIN) annotations from controller methods to adhere to Micronaut best practices

--- friendship-service/src/main/kotlin/piperkt/services/friendship/infrastructure/web/FriendsHTTPController.kt
+++ friendship-service/src/main/kotlin/piperkt/services/friendship/infrastructure/web/FriendsHTTPController.kt
@@ -5,7 +5,6 @@ import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
-import io.micronaut.http.annotation.Produces
 import io.micronaut.security.annotation.Secured
 import io.micronaut.security.authentication.Authentication
 import io.micronaut.security.rules.SecurityRule
@@ -19,21 +18,18 @@ class FriendsHTTPController(private val friendService: FriendService) {
     @Serdeable data class SendFriendRequest(val action: String, val to: String)
 
     @Get("/")
-    @Produces(TEXT_PLAIN)
     fun getFriends(authentication: Authentication): List<String> {
         val result = friendService.getFriends(authentication.name)
         return result
     }
 
     @Get("/request")
-    @Produces(TEXT_PLAIN)
     fun getFriendRequests(authentication: Authentication): List<String> {
         val result = friendService.getFriendRequests(authentication.name)
         return result
     }
 
     @Post("/request")
-    @Produces(TEXT_PLAIN)
     fun sendFriendRequest(authentication: Authentication, @Body request: SendFriendRequest) {
         when (request.action) {
             "send" -> friendService.sendFriendRequest(authentication.name, request.to)
