97a37a715a471b386404dd26f27e936d0f3865dd
Tommaso Patriti <tommaso.patriti@studio.unibo.it>
2024-04-01 11:10:31 +0200

    feat(DirectEntity): add DirectEntity and MessageEntity classes for storing direct messages in the database

--- friendship-service/src/main/kotlin/piperkt/services/friendship/application/DirectService.kt
+++ friendship-service/src/main/kotlin/piperkt/services/friendship/application/DirectService.kt
@@ -4,9 +4,13 @@ import piperkt.services.friendship.application.api.DirectServiceApi
 import piperkt.services.friendship.domain.DirectRepository
 import piperkt.services.friendship.domain.Message
 
-class DirectService(private val directRepository: DirectRepository): DirectServiceApi {
-    override fun getDirectMessagesPaginated(username: String, friendUsername: String, from: Int, limit: Int) =
-        directRepository.getDirectMessagesPaginated(username, friendUsername, from, limit)
+class DirectService(private val directRepository: DirectRepository) : DirectServiceApi {
+    override fun getDirectMessagesPaginated(
+        username: String,
+        friendUsername: String,
+        from: Int,
+        limit: Int
+    ) = directRepository.getDirectMessagesPaginated(username, friendUsername, from, limit)
 
     override fun createDirect(username: String, friendUsername: String) {
         directRepository.createDirect(username, friendUsername)
