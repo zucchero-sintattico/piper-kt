79ca3133850afe3ce531b7bd57bca43d10da05fc
Tommaso Patriti <tommaso.patriti@studio.unibo.it>
2024-04-01 11:40:48 +0200

    refactor(DirectEntity): change timestamp property type to String and set default value to current timestamp

--- friendship-service/src/main/kotlin/piperkt/services/friendship/infrastructure/persistence/model/DirectEntity.kt
+++ friendship-service/src/main/kotlin/piperkt/services/friendship/infrastructure/persistence/model/DirectEntity.kt
@@ -1,10 +1,9 @@
 package piperkt.services.friendship.infrastructure.persistence.model
-
 import io.micronaut.data.annotation.Id
 import io.micronaut.data.annotation.MappedEntity
 import io.micronaut.data.mongodb.annotation.MongoRepository
 import io.micronaut.data.repository.CrudRepository
-import kotlin.properties.Delegates
+import java.time.Instant
 
 @MappedEntity
 data class DirectEntity(
@@ -15,7 +14,8 @@ data class DirectEntity(
 class MessageEntity {
     lateinit var sender: String
     lateinit var content: String
-    var timestamp by Delegates.notNull<Long>()
+    var timestamp: String = Instant.now().toString()
 }
+
 
 @MongoRepository interface DirectModelRepository : CrudRepository<DirectEntity, String>
