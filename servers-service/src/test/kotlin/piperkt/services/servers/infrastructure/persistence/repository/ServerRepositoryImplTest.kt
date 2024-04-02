f8be0cbc98da51e9e684a0f3245e869f05dba35b
luigi-borriello00 <luigi.borriello2@studio.unibo.it>
2024-04-02 18:40:15 +0200

    refactor(packages): add packages in test suite

--- servers-service/src/test/kotlin/piperkt/services/servers/infrastructure/persistence/repository/ServerRepositoryImplTest.kt
+++ servers-service/src/test/kotlin/piperkt/services/servers/infrastructure/persistence/repository/ServerRepositoryImplTest.kt
@@ -1,4 +1,4 @@
-package piperkt.services.servers.infrastructure
+package piperkt.services.servers.infrastructure.persistence.repository
 
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
