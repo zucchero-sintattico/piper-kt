28554e8b6e93cfd57dcf2a17dbeec25d0a7c0db2
Ro0t-set <tommaso.patriti@studio.unibo.it>
2024-04-09 15:33:07 +0200

    feat(atlas-operator): add new helm chart files for mongodb atlas operator

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
+kubectl delete --all pods --namespace=atlas-operator
+kubectl delete --all pods --namespace=micronaut-k8s
+
 helm ls --all --short | xargs -L1 helm delete
 
 kubectl delete -f auth.yml
-kubectl apply -f auth.yml
+
 
 # shellcheck disable=SC2093
 eval $(minikube docker-env)
-./gradlew dockerBuild
-helm install  friendship-service  piper-chart  --values  friendship-service/values.yaml
+#./gradlew dockerBuild
 
+kubectl apply -f auth.yml
+
+helm upgrade -i  friendship-service  piper-chart  --values  friendship-service/values.yaml
+
+#https://github.com/mongodb/helm-charts/tree/d843c293bf86653d4b9193ab245f04934c93a722/charts/atlas-operator
+helm upgrade -i atlas-operator\
+  --namespace=atlas-operator\
+  --create-namespace atlas-operator\
+  --values friendship-service/db-values.yaml
