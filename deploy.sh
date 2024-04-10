819f7fdc52501107ed4bb35ebc719f5f7b5dbf5b
Ro0t-set <tommaso.patriti@studio.unibo.it>
2024-04-10 13:42:01 +0200

    feat(kubernetesNamespce): update namespace value to use dynamic namespace from values

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 
-kubectl delete --all pods --namespace=atlas-operator
-kubectl delete --all pods --namespace=micronaut-k8s
-
-helm ls --all --short | xargs -L1 helm delete
-
-kubectl delete -f auth.yml
+minikube delete
+minikube start
 
 
-# shellcheck disable=SC2093
 eval $(minikube docker-env)
-#./gradlew dockerBuild
+./gradlew dockerBuild
 
-kubectl apply -f auth.yml
-
-helm upgrade -i friendship-service  helm-chart/piper-chart  --values  friendship-service/values.yaml
 
 #https://github.com/mongodb/helm-charts/tree/d843c293bf86653d4b9193ab245f04934c93a722/charts/
+helm upgrade -i friendship-service  helm-chart/piper-chart  --values  friendship-service/helm-values/micronaut-values.yaml
 helm dependency build helm-chart/community-operator
-helm upgrade -i mongodb-friendship helm-chart/community-operator --namespace mongodb --create-namespace --values friendship-service/db-values.yaml
+helm upgrade -i friendship-service helm-chart/community-operator --namespace friendship --values friendship-service/helm-values/db-values.yaml
