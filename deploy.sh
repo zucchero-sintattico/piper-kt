399a5ffb28420fe08ab697b5aafbe0ddfa4523c1
Ro0t-set <tommaso.patriti@studio.unibo.it>
2024-04-11 10:49:37 +0200

    chore(kuberneteNamespace): update namespace from 'friendship' to 'piper-kt'

--- deploy.sh
+++ deploy.sh
@@ -11,4 +11,4 @@ eval $(minikube docker-env)
 #https://github.com/mongodb/helm-charts/tree/d843c293bf86653d4b9193ab245f04934c93a722/charts/
 helm upgrade -i friendship-service  helm-chart/piper-chart  --values  friendship-service/helm-values/micronaut-values.yaml
 helm dependency build helm-chart/community-operator
-helm upgrade -i friendship-service helm-chart/community-operator --namespace friendship --values friendship-service/helm-values/db-values.yaml
+helm upgrade -i friendship-service helm-chart/community-operator --namespace piper-kt --values friendship-service/helm-values/db-values.yaml
