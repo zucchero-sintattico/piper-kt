b678876f8edb62b18e57f610c6562dad68fe409e
Ro0t-set <tommaso.patriti@studio.unibo.it>
2024-04-11 11:05:27 +0200

    chore(deploy): update deploy script to install community-operator using helm online repository

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ eval $(minikube docker-env)
 
 #https://github.com/mongodb/helm-charts/tree/d843c293bf86653d4b9193ab245f04934c93a722/charts/
 helm upgrade -i friendship-service  helm-chart/piper-chart  --values  friendship-service/helm-values/micronaut-values.yaml
-helm dependency build helm-chart/community-operator
-helm upgrade -i friendship-service helm-chart/community-operator --namespace piper-kt --values friendship-service/helm-values/db-values.yaml
+#helm upgrade -i friendship-service helm-chart/community-operator --namespace piper-kt --values friendship-service/helm-values/db-values.yaml
+helm install community-operator mongodb/community-operator --namespace piper-kt --values friendship-service/helm-values/db-values.yaml
