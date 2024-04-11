6a2442808e93522902e9c25e89f0e1ca8944ba0a
Ro0t-set <tommaso.patriti@studio.unibo.it>
2024-04-11 14:44:49 +0200

    feat(mongo-for-operator): add values.yaml for configuration of MongoDB resources

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,9 @@ minikube start
 eval $(minikube docker-env)
 ./gradlew dockerBuild
 
-
+helm repo add mongodb https://mongodb.github.io/helm-charts
 #https://github.com/mongodb/helm-charts/tree/d843c293bf86653d4b9193ab245f04934c93a722/charts/
 helm upgrade -i friendship-service  helm-chart/piper-chart  --values  friendship-service/helm-values/micronaut-values.yaml
 #helm upgrade -i friendship-service helm-chart/community-operator --namespace piper-kt --values friendship-service/helm-values/db-values.yaml
-helm install community-operator mongodb/community-operator --namespace piper-kt --values friendship-service/helm-values/db-values.yaml
+helm upgrade -i --set image.tag=0.9.0-arm64 community-operator mongodb/community-operator --namespace piper-kt --set watchNamespaces=piper-kt
+helm upgrade -i friendship-service  helm-chart/mongo-for-operator --namespace piper-kt
