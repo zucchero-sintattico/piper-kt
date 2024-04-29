7df45f72b20265ed450c70fc20a325ec0fcae333
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2024-04-29 13:57:20 +0200

    test(users-service): added integration tests for login

--- users-service/src/test/kotlin/piperkt/services/users/interfaces/web/auth/RegisterControllerTest.kt
+++ users-service/src/test/kotlin/piperkt/services/users/interfaces/web/auth/RegisterControllerTest.kt
@@ -3,22 +3,24 @@ package piperkt.services.users.interfaces.web.auth
 import base.IntegrationTest
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.shouldBe
-import io.micronaut.http.HttpHeaders
+import io.kotest.matchers.shouldNotBe
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Body
-import io.micronaut.http.annotation.Header
 import io.micronaut.http.annotation.Post
 import io.micronaut.http.client.annotation.Client
 import io.micronaut.http.client.exceptions.HttpClientResponseException
+import io.micronaut.security.authentication.UsernamePasswordCredentials
+import io.micronaut.security.token.render.BearerAccessRefreshToken
 import piperkt.services.users.presentation.user.UserDTO
 
 @Client("/")
 interface AuthClient {
     @Post("/register") fun register(@Body request: RegisterController.RegisterRequest): UserDTO
 
-    @Post("/login") fun login(username: String, password: String)
-
-    @Post("/logout") fun logout(@Header(HttpHeaders.AUTHORIZATION) authorization: String)
+    @Post("/login")
+    fun login(
+        @Body usernamePasswordCredentials: UsernamePasswordCredentials
+    ): BearerAccessRefreshToken
 }
 
 class RegisterControllerTest(private val authClient: AuthClient) :
@@ -41,5 +43,31 @@ class RegisterControllerTest(private val authClient: AuthClient) :
                 }
         }
 
-        test("login") { authClient.login("user", "password") }
+        test("login") {
+            val response = authClient.login(UsernamePasswordCredentials("user", "password"))
+            response.accessToken shouldNotBe null
+            response.refreshToken shouldNotBe null
+            response.username shouldBe "user"
+            response.roles shouldBe null
+        }
+
+        test("login with invalid password should throw AuthenticationException") {
+            shouldThrow<HttpClientResponseException> {
+                    authClient.login(UsernamePasswordCredentials("user", "invalid"))
+                }
+                .let {
+                    it.status shouldBe HttpStatus.UNAUTHORIZED
+                    it.message shouldBe "Credentials Do Not Match"
+                }
+        }
+
+        test("login with invalid username should throw AuthenticationException") {
+            shouldThrow<HttpClientResponseException> {
+                    authClient.login(UsernamePasswordCredentials("invalid", "password"))
+                }
+                .let {
+                    it.status shouldBe HttpStatus.UNAUTHORIZED
+                    it.message shouldBe "User Not Found"
+                }
+        }
     })
