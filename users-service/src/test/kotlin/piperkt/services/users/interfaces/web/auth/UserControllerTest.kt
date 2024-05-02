4da3ae13ddadd028be3e63f177f5655b9d140000
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2024-05-02 11:32:09 +0200

    test(users-service): tested whoami and getUser endpoint

--- users-service/src/test/kotlin/piperkt/services/users/interfaces/web/auth/UserControllerTest.kt
+++ users-service/src/test/kotlin/piperkt/services/users/interfaces/web/auth/UserControllerTest.kt
@@ -5,24 +5,41 @@ import io.kotest.matchers.shouldBe
 import io.micronaut.http.HttpHeaders
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Header
+import io.micronaut.http.annotation.PathVariable
 import io.micronaut.http.client.annotation.Client
+import piperkt.services.users.application.AuthService
 import piperkt.services.users.interfaces.web.auth.TestUtils.authOf
+import piperkt.services.users.presentation.user.UserDTO
+import piperkt.services.users.presentation.user.UserMapper.toDTO
 
 @Client("/")
 interface UserControllerClient {
-    val auth: String
-        get() = authOf("user")
 
-    @Get("whoami") fun whoami(@Header(HttpHeaders.AUTHORIZATION) authorization: String = auth)
+    @Get("whoami") fun whoami(@Header(HttpHeaders.AUTHORIZATION) authorization: String): UserDTO
 
     @Get("users/{username}")
-    fun getUser(username: String, @Header(HttpHeaders.AUTHORIZATION) authorization: String = auth)
+    fun getUser(
+        @PathVariable username: String,
+        @Header(HttpHeaders.AUTHORIZATION) authorization: String
+    ): UserDTO
 }
 
-class UserControllerTest(private val userControllerClient: UserControllerClient) :
+class UserControllerTest(
+    private val userControllerClient: UserControllerClient,
+    private val authService: AuthService
+) :
     IntegrationTest.FunSpec({
+        val user = authService.register("user", "password")
+        val otherUser = authService.register("otherUser", "password")
+
         test("whoami") {
-            val response = userControllerClient.whoami()
-            response shouldBe "user"
+            val response = userControllerClient.whoami(authOf(user.username.value))
+            response shouldBe user.toDTO()
+        }
+
+        test("getUser") {
+            val response =
+                userControllerClient.getUser(otherUser.username.value, authOf(user.username.value))
+            response shouldBe otherUser.toDTO()
         }
     })
