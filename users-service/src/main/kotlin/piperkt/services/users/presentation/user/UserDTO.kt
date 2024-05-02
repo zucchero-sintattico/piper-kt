7a2da0009ef350ebd3a6c1c8feae085ac907e67d
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2024-05-02 11:32:09 +0200

    test(users-service): tested whoami and getUser endpoint

--- users-service/src/main/kotlin/piperkt/services/users/presentation/user/UserDTO.kt
+++ users-service/src/main/kotlin/piperkt/services/users/presentation/user/UserDTO.kt
@@ -7,4 +7,24 @@ data class UserDTO(
     val username: String,
     val description: String = "",
     val profilePicture: ByteArray = byteArrayOf()
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as UserDTO
+
+        if (username != other.username) return false
+        if (description != other.description) return false
+        if (!profilePicture.contentEquals(other.profilePicture)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = username.hashCode()
+        result = 31 * result + description.hashCode()
+        result = 31 * result + profilePicture.contentHashCode()
+        return result
+    }
+}
