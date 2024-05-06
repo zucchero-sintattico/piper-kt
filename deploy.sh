2832565a94cbaaf0a91a4b783966f1bdaae0ae44
Ro0t-set <tommaso.patriti@studio.unibo.it>
2024-05-06 13:14:52 +0200

    feat(deploy): update helm install commands to install microservices with specific values

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
 minikube delete
-minikube config set memory 2048
+minikube config set memory 4098
 minikube config set cpus 4
 minikube start
 
 
 eval $(minikube docker-env)
 ./gradlew dockerBuild
+#create namespace
+kubectl create namespace piper-kt
+kubectl apply -f auth.yml
 helm repo add mongodb https://mongodb.github.io/helm-charts
 microservice_list=("friendships-service" "users-service" "servers-service")
 
 for microservice in "${microservice_list[@]}"
 do
-  helm upgrade -i $microservice helm-chart/piper-chart --values $microservice/helm-values/micronaut-values.yaml
+  helm install $microservice helm-chart/piper-chart --values $microservice/helm-values/micronaut-values.yaml
 
-  helm upgrade -i --set image.tag=0.9.0-arm64 community-operator mongodb/community-operator --namespace piper-kt \
-      --set watchNamespaces=piper-kt\
-      --values $microservice/helm-values/db-values.yaml
-
-  helm upgrade -i $microservice helm-chart/mongo-for-operator --namespace piper-kt\
-      --values $microservice/helm-values/mongo-values.yaml
+#  helm install --set image.tag=0.9.0-arm64 community-operator mongodb/community-operator --namespace piper-kt \
+#      --set watchNamespaces=piper-kt\
+#      --values $microservice/helm-values/db-values.yaml
+#
+#  helm install $microservice helm-chart/mongo-for-operator --namespace piper-kt\
+#      --values $microservice/helm-values/mongo-values.yaml
 
 done
 
