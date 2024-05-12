232250593476550af48a20218c056d70b6fbfa91
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2024-05-12 10:17:24 +0200

    feat(frontend): gradlifing the project and setup delivery tasks

--- frontend-service/build.gradle.kts
+++ frontend-service/build.gradle.kts
@@ -1,10 +1,13 @@
+import io.github.zuccherosintattico.gradle.BuildCommandExecutable
+
 plugins {
-    id("io.github.zucchero-sintattico.typescript-gradle-plugin") version "4.2.2"
+    id("io.github.zucchero-sintattico.typescript-gradle-plugin") version "4.3.0"
     alias(libs.plugins.spotless)
 }
 
 typescript {
-    entrypoint = "app.js"
+    buildCommandExecutable = BuildCommandExecutable.NPM
+    buildCommand = "run build"
 }
 
 node {
@@ -17,5 +20,28 @@ spotless {
         target("**/*.ts")
         targetExclude("**/build/**", "**/node_modules/**")
         prettier()
+    }
+}
+
+tasks.register("buildLayers") {
+    dependsOn("compileTypescript")
+    doLast {
+        copy {
+            from("dist")
+            into("build/docker/main/dist")
+        }
+        copy {
+            from("node_modules")
+            into("build/docker/main/node_modules")
+        }
+    }
+}
+
+tasks.register("dockerfile") {
+    doLast {
+        copy {
+            from("Dockerfile")
+            into("build/docker/main")
+        }
     }
 }
