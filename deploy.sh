2d1eb127eb56bb48ee8851dab932b5015a5703f3
Ro0t-set <tommaso.patriti@studio.unibo.it>
2024-05-14 10:55:15 +0200

    feat(application): add Kafka configuration for all services to connect to cluster-kafka-brokers.piper-kt.svc.cluster.local:9092 for messaging functionality

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-# This script is used to deploy the application to a kubernetes cluster
-
-
-
 
 #create namespace
 kubectl create namespace piper-kt
 kubectl apply -f kubernetes/auth.yml
 helm repo add mongodb https://mongodb.github.io/helm-charts
 
-#kubectl create -f 'https://strimzi.io/install/latest?namespace=piper-kt' -n piper-kt
-#kubectl apply -f kubernetes/kafka.yml --namespace piper-kt
+
 
 helm install strimzi-cluster-operator --set replicas=1 --set resources.limits.cpu=500 oci://quay.io/strimzi-helm/strimzi-kafka-operator --namespace piper-kt
 kubectl apply -f kubernetes/kafka.yml --namespace piper-kt
@@ -21,8 +16,8 @@ helm install --set image.tag=0.9.0-arm64 community-operator mongodb/community-op
 
 
 ./gradlew dockerBuild
-#microservice_list=("friendships-service" "users-service" "servers-service" "multimedia-service")
-microservice_list=("users-service")
+microservice_list=("friendships-service" "users-service" "servers-service" "multimedia-service")
+#microservice_list=("users-service")
 
 for microservice in "${microservice_list[@]}"
 do
@@ -41,18 +36,5 @@ kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/cont
 sleep 90
 
 kubectl port-forward svc/ingress-nginx-controller 8080:80 -n ingress-nginx
-#minikube addons enable ingress
 
-#minikube tunnel
-
-
-#https://github.com/mongodb/helm-charts/tree/d843c293bf86653d4b9193ab245f04934c93a722/charts/
-
-
-
-#kubectl port-forward services/friendships-mongo-svc -n piper-kt  27017:27017
-#Connection String: "mongodb://friendships-mongo:password@localhost:27017/?authSource=admin&authMechanism=SCRAM-SHA-256&directConnection=true"
-
-#expose nginx service
-#kubectl port-forward services/services-nginx-ingress-controller -n piper-kt  8080:80
 
