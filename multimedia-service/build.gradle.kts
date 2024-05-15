0869e0290a4cd857a326f9896ce85df5b6e9fcea
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2024-05-15 14:37:09 +0200

    fix(jar): add zip64 extensions for other microservices build

--- multimedia-service/build.gradle.kts
+++ multimedia-service/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     id("micronaut-full")
 }
@@ -7,6 +9,10 @@ application { mainClass.set("piperkt.services.multimedia.ApplicationKt") }
 dependencies {
     implementation("com.corundumstudio.socketio:netty-socketio:2.0.9")
     implementation("io.socket:socket.io-client:2.1.0")
+}
+
+tasks.withType<ShadowJar> {
+    isZip64= true
 }
 
 detekt { config.from(files("detekt-config.yml")) }
