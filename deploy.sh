144ab118d4798fd2447c30d6d2016722914a3186
Ro0t-set <tommaso.patriti@studio.unibo.it>
2024-05-20 13:11:50 +0200

    feat(user): simplify logout function by removing unnecessary try-catch block and directly clear localStorage on logout

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-#eval $(minikube docker-env)
-./gradlew clean
-#./gradlew dockerBuild
-
-gradle :frontend-service:dockerfile :frontend-service:buildLayers
-docker build -t frontend-service ./frontend-service/build/docker/main
 
 kubectl create namespace piper-kt
 kubectl apply -f kubernetes/auth.yml
@@ -16,7 +10,9 @@ helm install --set image.tag=0.9.0-arm64 community-operator mongodb/community-op
     --set watchNamespaces=piper-kt\
     --values kubernetes/operator-values.yaml
 
+
 microservice_list=("friendships-service" "users-service" "servers-service" "multimedia-service")
+#microservice_list=("users-service")
 for microservice in "${microservice_list[@]}"
 do
   helm install $microservice kubernetes/helm-chart/piper-chart --values $microservice/helm-values/micronaut-values.yaml
@@ -24,17 +20,12 @@ do
       --values $microservice/helm-values/mongo-values.yaml
 done
 
-
-
-#kubectl create deployment frontend-service --image=zuccherosintattico/piperkt-frontend-service --namespace piper-kt
-
-
 kubectl apply -f kubernetes/frontend.yml --namespace piper-kt
 
 helm install nginx-ingress-controller kubernetes/helm-chart/piper-ingress --namespace piper-kt
 sleep 5
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.10.1/deploy/static/provider/aws/deploy.yaml
-sleep 90
+sleep 60
 kubectl get pods -n piper-kt
 kubectl get deploy -n piper-kt
 kubectl get svc -n piper-kt
