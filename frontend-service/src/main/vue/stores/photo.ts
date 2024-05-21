cc225d6cbd3d30492cfc01e28808540f21854e02
Ro0t-set <tommaso.patriti@studio.unibo.it>
2024-05-22 00:41:28 +0200

    fix(vue): change multimedia to MULTIMEDIA. Remove unnecessary usePhotoStore to simplify the code.

--- frontend-service/src/main/vue/stores/photo.ts
+++ frontend-service/src/main/vue/stores/photo.ts
@@ -8,33 +8,9 @@ export const usePhotoStore = defineStore("photo", () => {
   const userController: UserController = new UserControllerImpl();
   const usersPhotos = ref<Record<string, string | undefined>>({});
 
-  async function reloadUserPhoto(targetUsername: string) {
-    try {
-      const response = await userController.getUserPhoto({
-        username: targetUsername,
-      });
-      if (response.statusCode === 200) {
-        const typed = response as GetUserPhotoApi.Responses.Success;
-        if (typed.photo.data !== undefined) {
-          usersPhotos.value[targetUsername] =
-            "data:image/jpeg;base64," +
-            btoa(
-              new Uint8Array((typed.photo.data as any).data).reduce(
-                (data, byte) => data + String.fromCharCode(byte),
-                ""
-              )
-            );
-        }
-      }
-    } catch (e) {
-      console.log(e);
-    }
-  }
+  async function reloadUserPhoto(targetUsername: string) {}
 
   function getUserPhoto(targetUsername: string): Ref<string | undefined> {
-    if (usersPhotos.value[targetUsername] === undefined) {
-      reloadUserPhoto(targetUsername);
-    }
     return computed(() => usersPhotos.value[targetUsername]);
   }
 
