1e47de67a6bd21336257031dd9ba72f99883a3f5
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2024-05-22 21:28:47 +0200

    refactor(notifications): delete unsed and change built lib name

--- notifications-service/src/main/typescript/commons/healthcheck/router.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-const healthCheckRouter = Router();
-
-healthCheckRouter.get("/health", (req, res) => {
-  res.status(200).send("OK");
-});
-
-export { healthCheckRouter };
