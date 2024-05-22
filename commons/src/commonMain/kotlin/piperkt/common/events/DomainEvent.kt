944230bcdb92e9794a401118df96c2e049f481d1
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2024-05-22 20:17:18 +0200

    chore(kotlin-js): add message type and export it

--- commons/src/commonMain/kotlin/piperkt/common/events/DomainEvent.kt
+++ commons/src/commonMain/kotlin/piperkt/common/events/DomainEvent.kt
@@ -1,5 +1,11 @@
 package piperkt.common.events
 
 import piperkt.common.ddd.ValueObject
+import kotlin.js.ExperimentalJsExport
+import kotlin.js.JsExport
 
-interface DomainEvent : ValueObject
+@OptIn(ExperimentalJsExport::class)
+@JsExport
+interface DomainEvent : ValueObject {
+    val type: String get() = this::class.simpleName!!
+}
