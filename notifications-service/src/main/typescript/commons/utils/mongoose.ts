f01d99345c532c7e24a2aee28d1bb649baf5274b
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2024-05-23 22:32:57 +0200

    chore(notifications): let throw error if service dependencies are not fulfilled

--- notifications-service/src/main/typescript/commons/utils/mongoose.ts
+++ notifications-service/src/main/typescript/commons/utils/mongoose.ts
@@ -2,12 +2,8 @@ import { Mongoose } from "mongoose";
 
 export class MongooseUtils {
   static async initialize(mongoose: Mongoose, connectionUri: string) {
-    try {
-      await mongoose.connect(connectionUri);
-      console.log("Connected to MongoDB");
-    } catch (err) {
-      console.error(err);
-    }
+    await mongoose.connect(connectionUri);
+    console.log("Connected to MongoDB");
   }
 
   static async close(mongoose: Mongoose) {
