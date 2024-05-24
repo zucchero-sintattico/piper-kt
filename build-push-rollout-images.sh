9bdd5bacef760fbc6838e0274da59b48e2586a07
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2024-05-24 17:11:20 +0200

    build: add command for build push and rollout local images

---
+++ build-push-rollout-images.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+./gradlew clean
+
+for service in "${@:2}"
+do
+  echo "Building $service"
+  ./gradlew clean :$service:dockerfile :$service:buildLayers
+  docker buildx build -t zuccherosintattico/piperkt-$service:test --platform=linux/amd64 --push ./$service/build/docker/main/
+  ssh $1 "kubectl rollout restart deployment $service"
+done
