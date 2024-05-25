fc5c035f56a297cf2a386ddb011fe8dadd0af5ec
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2024-05-25 14:13:55 +0200

    fix(events-serialization): added serializare for events in servers and friendships services

--- friendships-service/src/main/kotlin/piperkt/services/friendships/infrastructure/events/SerializableFriendshipEvents.kt
+++ friendships-service/src/main/kotlin/piperkt/services/friendships/infrastructure/events/SerializableFriendshipEvents.kt
@@ -1,6 +1,13 @@
 package piperkt.services.friendships.infrastructure.events
 
 import io.micronaut.serde.annotation.SerdeImport
-import piperkt.events.FriendshipEvent
+import piperkt.events.FriendshipRequestAcceptedEvent
+import piperkt.events.FriendshipRequestRejectedEvent
+import piperkt.events.FriendshipRequestSentEvent
+import piperkt.events.NewMessageInFriendshipEvent
 
-@SerdeImport(FriendshipEvent::class) object SerializableFriendshipEvents
+@SerdeImport(FriendshipRequestSentEvent::class)
+@SerdeImport(FriendshipRequestAcceptedEvent::class)
+@SerdeImport(FriendshipRequestRejectedEvent::class)
+@SerdeImport(NewMessageInFriendshipEvent::class)
+object SerializableFriendshipEvents
