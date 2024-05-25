b9b8dc558ec0938005c1dc0600b3d1babf83356a
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2024-05-25 14:13:55 +0200

    fix(events-serialization): added serializare for events in servers and friendships services

--- servers-service/src/main/kotlin/piperkt/services/servers/infrastructure/events/SerializableEvents.kt
+++ servers-service/src/main/kotlin/piperkt/services/servers/infrastructure/events/SerializableEvents.kt
@@ -1,9 +1,27 @@
 package piperkt.services.servers.infrastructure.events
 
 import io.micronaut.serde.annotation.SerdeImport
-import piperkt.events.ChannelEvent
-import piperkt.events.ServerEvent
+import piperkt.events.ChannelCreatedEvent
+import piperkt.events.ChannelDeletedEvent
+import piperkt.events.ChannelUpdatedEvent
+import piperkt.events.MessageInChannelEvent
+import piperkt.events.ServerCreatedEvent
+import piperkt.events.ServerDeletedEvent
+import piperkt.events.ServerUpdatedEvent
+import piperkt.events.ServerUserAddedEvent
+import piperkt.events.ServerUserKickedEvent
+import piperkt.events.ServerUserRemovedEvent
 
-@SerdeImport(ServerEvent::class) object SerializableServerEvents
+@SerdeImport(ServerCreatedEvent::class)
+@SerdeImport(ServerUpdatedEvent::class)
+@SerdeImport(ServerUserAddedEvent::class)
+@SerdeImport(ServerUserRemovedEvent::class)
+@SerdeImport(ServerUserKickedEvent::class)
+@SerdeImport(ServerDeletedEvent::class)
+object SerializableServerEvents
 
-@SerdeImport(ChannelEvent::class) object SerializableChannelEvents
+@SerdeImport(ChannelCreatedEvent::class)
+@SerdeImport(ChannelDeletedEvent::class)
+@SerdeImport(ChannelUpdatedEvent::class)
+@SerdeImport(MessageInChannelEvent::class)
+object SerializableChannelEvents
