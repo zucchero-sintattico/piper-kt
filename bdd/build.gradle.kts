c1d9ad80fc68bd935becd5bb4a8992dd09396656
luigi-borriello00 <luigi.borriello2@studio.unibo.it>
2024-05-27 11:13:13 +0200

    chore: add first files to test cucumber

--- bdd/build.gradle.kts
+++ bdd/build.gradle.kts
@@ -1,11 +1,15 @@
 plugins {
     id("kotlin-jvm")
+
 }
 
 dependencies {
     testImplementation(platform(libs.cucumber.bom))
     testImplementation(libs.bundles.cucumberJunit)
     testImplementation(libs.junit.platform)
+    testImplementation(project(":commons"))
+    testImplementation(project(":servers-service"))
+
 }
 
 // https://github.com/cucumber/cucumber-jvm/tree/main/cucumber-junit-platform-engine#gradle
--- bdd/src/test/resources/piperkt/bdd/server-management.feature
+++ bdd/src/test/resources/piperkt/bdd/server-management.feature
@@ -1,7 +1,7 @@
 Feature: Server Management
 
   Scenario: User creates a new server
-    Given the user is authenticated in the system
+    Given an authenticated user in the system
     When the user creates a new server with the name "MyServer"
     Then the server "MyServer" should be created successfully
 
