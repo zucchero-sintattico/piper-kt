d242d3be97e19f81ea31d9211b6a8c25a5e12ea5
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2024-05-27 13:09:24 +0200

    fix(bdd): add missing deps and correct package name

--- bdd/src/test/kotlin/piperkt/bdd/RunCucumberTest.kt
+++ bdd/src/test/kotlin/piperkt/bdd/RunCucumberTest.kt
@@ -1,16 +1,14 @@
 package piperkt.bdd
 
-// Uncomment this to run Cucumber tests via gradle test
-
 import io.cucumber.junit.platform.engine.Constants
 import org.junit.platform.suite.api.ConfigurationParameter
 import org.junit.platform.suite.api.IncludeEngines
-import org.junit.platform.suite.api.SelectClasspathResource
+import org.junit.platform.suite.api.SelectPackages
 import org.junit.platform.suite.api.Suite
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("piperchat/bdd")
+@SelectPackages("piperkt.bdd")
 @ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME, value = "pretty")
-@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "piperchat.bdd")
+@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "piperkt.bdd")
 class RunCucumberTest
