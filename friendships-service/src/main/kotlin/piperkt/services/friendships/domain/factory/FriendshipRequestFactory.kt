088f0f581a68f2b5b350233c66e64738dbfcedc0
luigi-borriello00 <luigi.borriello2@studio.unibo.it>
2024-06-02 17:03:43 +0200

    refactor(friendship-service): add Factory extension in FriendshipFactory and FriendshipRequestFactory

--- friendships-service/src/main/kotlin/piperkt/services/friendships/domain/factory/FriendshipRequestFactory.kt
+++ friendships-service/src/main/kotlin/piperkt/services/friendships/domain/factory/FriendshipRequestFactory.kt
@@ -1,8 +1,9 @@
 package piperkt.services.friendships.domain.factory
 
+import piperkt.common.ddd.Factory
 import piperkt.services.friendships.domain.FriendshipRequest
 
-object FriendshipRequestFactory {
+object FriendshipRequestFactory : Factory<FriendshipRequest> {
 
     fun createFriendshipRequest(from: String, to: String) = FriendshipRequest(from = from, to = to)
 }
