8a31278f359c225a983b42f03b85a58091a7dbd3
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2024-06-17 15:31:08 +0200

    test(bdd): added users-management tests

--- bdd/src/test/kotlin/piperkt/bdd/users/UserManagementSteps.kt
+++ bdd/src/test/kotlin/piperkt/bdd/users/UserManagementSteps.kt
@@ -4,94 +4,95 @@ import io.cucumber.java.en.And
 import io.cucumber.java.en.Given
 import io.cucumber.java.en.Then
 import io.cucumber.java.en.When
-import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
-import io.micronaut.http.HttpHeaders
-import io.micronaut.http.annotation.Body
-import io.micronaut.http.annotation.Get
-import io.micronaut.http.annotation.Header
-import io.micronaut.http.annotation.PathVariable
-import io.micronaut.http.annotation.Post
+import io.micronaut.http.HttpRequest
 import io.micronaut.http.client.HttpClient
-import io.micronaut.http.client.annotation.Client
 import io.micronaut.security.authentication.UsernamePasswordCredentials
 import io.micronaut.security.token.render.BearerAccessRefreshToken
 import io.micronaut.serde.annotation.Serdeable
 import io.micronaut.test.extensions.kotest5.annotation.MicronautTest
 import java.net.URL
+import java.util.*
 
-@Client("/")
-interface UserControllerClient {
+@Serdeable
+data class UserDTO(
+    val username: String,
+    val email: String,
+    val description: String,
+    val profilePicture: String?,
+)
 
-    @Serdeable
-    data class UserDTO(
-        val username: String,
-        val email: String,
-        val description: String,
-        val profilePicture: String?,
-    )
-
-    @Get("whoami") fun whoami(@Header(HttpHeaders.AUTHORIZATION) authorization: String): UserDTO
-
-    @Get("users/{username}")
-    fun getUser(
-        @PathVariable username: String,
-        @Header(HttpHeaders.AUTHORIZATION) authorization: String,
-    ): UserDTO
-
-    @Serdeable
-    data class RegisterRequest(
-        val username: String,
-        val password: String,
-        val email: String?,
-        val description: String?,
-    )
-
-    @Post("/auth/register") fun register(@Body request: RegisterRequest): UserDTO
-
-    @Post("/auth/login")
-    fun login(
-        @Body usernamePasswordCredentials: UsernamePasswordCredentials,
-    ): BearerAccessRefreshToken
-}
+@Serdeable
+data class RegisterRequest(
+    val username: String,
+    val password: String,
+    val email: String?,
+    val description: String?,
+)
 
 @MicronautTest
 class UserManagementSteps {
 
-    var httpClient = HttpClient.create(URL("http://localhost:8080"))
+    var httpClient = HttpClient.create(URL("http://localhost:10000"))
 
-    val templateUser = UserControllerClient.UserDTO("user", "email", "description", null)
-    var user: UserControllerClient.UserDTO? = null
+    var user: UserDTO? = null
     var token: BearerAccessRefreshToken? = null
+
+    fun randomUsername() = UUID.randomUUID().toString()
 
     @Given("I am not logged in")
     fun iAmNotLoggedIn() {
-        user = null
         token = null
     }
 
     @When("I make a REGISTER request with valid credentials")
     fun iMakeAREGISTERRequestWithValidCredentials() {
-        user = httpClient.toBlocking().exchange(Http)
+        user =
+            httpClient
+                .toBlocking()
+                .exchange(
+                    HttpRequest.POST(
+                        "/auth/register",
+                        RegisterRequest(randomUsername(), "password", "email", "description")
+                    ),
+                    UserDTO::class.java
+                )
+                .body()
     }
 
     @Then("I should be registered to the system")
     fun iShouldBeRegisteredToTheSystem() {
-        true shouldBe false
         user shouldNotBe null
     }
 
     @Given("I am registered")
     fun iAmRegistered() {
         user =
-            userControllerClient.register(
-                UserControllerClient.RegisterRequest("user", "password", "email", "description")
-            )
+            httpClient
+                .toBlocking()
+                .exchange(
+                    HttpRequest.POST(
+                        "/auth/register",
+                        RegisterRequest(randomUsername(), "password", "email", "description")
+                    ),
+                    UserDTO::class.java
+                )
+                .body()
     }
 
     @When("I make a LOGIN request with valid credentials")
     fun iMakeALOGINRequestWithValidCredentials() {
-        token = userControllerClient.login(UsernamePasswordCredentials("user", "password"))
+        token =
+            httpClient
+                .toBlocking()
+                .exchange(
+                    HttpRequest.POST(
+                        "/auth/login",
+                        UsernamePasswordCredentials(user!!.username, "password")
+                    ),
+                    BearerAccessRefreshToken::class.java
+                )
+                .body()
     }
 
     @Then("I should be logged in to the system")
@@ -102,9 +103,26 @@ class UserManagementSteps {
     @And("I am logged in")
     fun iAmLoggedIn() {
         user =
-            userControllerClient.register(
-                UserControllerClient.RegisterRequest("user", "password", "email", "description")
-            )
-        token = userControllerClient.login(UsernamePasswordCredentials("user", "password"))
+            httpClient
+                .toBlocking()
+                .exchange(
+                    HttpRequest.POST(
+                        "/auth/register",
+                        RegisterRequest(randomUsername(), "password", "email", "description")
+                    ),
+                    UserDTO::class.java
+                )
+                .body()
+        token =
+            httpClient
+                .toBlocking()
+                .exchange(
+                    HttpRequest.POST(
+                        "/auth/login",
+                        UsernamePasswordCredentials(user!!.username, "password")
+                    ),
+                    BearerAccessRefreshToken::class.java
+                )
+                .body()
     }
 }
